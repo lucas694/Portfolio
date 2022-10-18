2cbf45b55355fc207ff1d0b31a4ede3f0c095d5f
lucas694 <lucasassuncao694@gmail.com>
2022-10-17 22:49:44 -0300

    att

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,17 @@
 import "./Header.css"
 import Dath from "../assets/img/darth.png";
-import { render } from "react-dom";
 import { motion, useTime, useTransform } from "framer-motion";
+import { FaBars } from "react-icons/fa";
+import { useState } from "react";
+
 
 const Header = ()=>{
   const time = useTime();
   const rotate = useTransform(time, [0, 4000], [0, 360], { clamp: false });
+
+  const [isOpen, setIsOpen] = useState(false);
+  const OpenNav = () => setIsOpen(!isOpen);
+
 
   return(
       <div className="HeaderContent">
@@ -16,14 +22,25 @@ const Header = ()=>{
         </div>
 
         <div className="HeaderMenu">
-          <ul>
-            <li><a href="#Inicio">Inicio</a></li>
-            <li><a href="#SobreMim">Sobre Mim</a></li>
-            <li><a href="#Projetos">Projetos</a></li>
-            <li><a href="#Contato">Contato</a></li>
-          </ul>
-        </div>
+          <button onClick={OpenNav}>
+            <FaBars className={"NavMobileIcon"}/>
+          </button>
 
+
+
+            <ul>
+              <li><a href="#Inicio">Inicio</a></li>
+              <li><a href="#SobreMim">Sobre Mim</a></li>
+              <li><a href="#Projetos">Projetos</a></li>
+              <li><a href="#Contato">Contato</a></li>
+            </ul>
+        </div>
+        <div className={`NavbarMobile ${isOpen ? "NavOpen" : "NavClose"}`} >
+          <a href="#Inicio">Inicio</a>
+          <a href="#SobreMim">Sobre Mim</a>
+          <a href="#Projetos">Projetos</a>
+          <a href="#Contato">Contato</a>
+        </div>
       </div>
   )
 }
