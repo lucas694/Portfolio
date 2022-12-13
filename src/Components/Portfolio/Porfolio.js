9712b4bb4579a15b69328b443979ee044bd5836d
lucas694 <lucasassuncao694@gmail.com>
2022-12-13 15:28:15 -0300

    att

--- src/Components/Portfolio/Porfolio.js
+++ src/Components/Portfolio/Porfolio.js
@@ -8,6 +8,7 @@ import Nft from  "../../assets/img/Portfolio/Nft.png"
 import Banking from  "../../assets/img/Portfolio/Banking.png"
 import CommingSoon from  "../../assets/img/Portfolio/Comming-Soon.png"
 import NftMarket from  "../../assets/img/Portfolio/NftMarket.png"
+import Gow from  "../../assets/img/Portfolio/Gow.png"
 import Porfolio from  "../../assets/img/Portfolio/Porfolio.png"
 import { motion } from "framer-motion"
 
@@ -36,8 +37,10 @@ const Portfolio = () => {
     {id:4 , tittle:"Rick and Morty API", img:RnM, link:"https://rick-and-morty-six-khaki.vercel.app",GitLink:"https://github.com/lucas694/Rick-and-Morty"},
     {id:5 , tittle:"Ladding Page NFT", img:Nft, link:"https://ladding-page-nft.vercel.app",GitLink:"https://github.com/lucas694/Ladding-page-Nft"},
     {id:6 , tittle:"Banking Page", img:Banking, link:"https://bank-project-alpha.vercel.app",GitLink:"https://github.com/lucas694/Bank-Project"},
-    {id:7 , tittle:"Comming Soon", img:CommingSoon, link:"https://commig-soon-page.vercel.app",GitLink:"https://github.com/lucas694/CommigSoonPage"},
-    {id:8 , tittle:"NFT Marketplace", img:NftMarket, link:"https://nft-marketplace-drab-six.vercel.app",GitLink:"https://github.com/lucas694/Nft-Marketplace"},
+    {id:7 , tittle:"NFT Marketplace", img:NftMarket, link:"https://nft-marketplace-drab-six.vercel.app",GitLink:"https://github.com/lucas694/Nft-Marketplace"},
+    {id:8 , tittle:"God of War Page", img:Gow, link:"https://gow-ruddy.vercel.app",GitLink:"https://github.com/lucas694/Gow"},
+    {id:9 , tittle:"Comming Soon", img:CommingSoon, link:"https://commig-soon-page.vercel.app",GitLink:"https://github.com/lucas694/CommigSoonPage"},
+
   ]
   return (
     <motion.div className="PortfolioContainer" id={"Projetos"}
