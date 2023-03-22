8eda05d860c41f9d39cbfad8fcefdc0ebf52190a
lucas694 <lucasassuncao694@gmail.com>
2023-03-22 12:56:32 -0300

    att

--- src/Components/Conhecimentos/Conhecimentos.js
+++ src/Components/Conhecimentos/Conhecimentos.js
@@ -20,7 +20,7 @@ const Conhecimentos = (props) => {
     {id:3,langName:"React", langIcon:<FaReact className={"Icon"}/>, langDescription:"ReactJS é uma biblioteca JavaScript de código aberto com foco em criar interfaces de usuário em páginas web.",},
     {id:4,langName:"JavaScript", langIcon:<FaJsSquare className={"Icon"}/>, langDescription:"JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma.",},
     {id:5,langName:"GIT", langIcon:<FaGitAlt className={"Icon"}/>, langDescription:"Git é um sistema de controle de versão distribuído, usado principalmente no desenvolvimento de software, mas pode ser usado para registrar o histórico de edições de qualquer tipo de arquivo.",},
-    {id:6,langName:"Tailwind CSS", langIcon:<SiTailwindcss className={"Icon"}/>, langDescription:"Tailwind CSS é um framework desenvolvido para maximizar o potencial do bom e velho CSS e levá-lo ainda mais longe.",},
+    {id:6,langName:"Tailwind", langIcon:<SiTailwindcss className={"Icon"}/>, langDescription:"Tailwind CSS é um framework desenvolvido para maximizar o potencial do bom e velho CSS e levá-lo ainda mais longe.",},
     {id:7,langName:"Redux ", langIcon:<SiRedux className={"Icon"}/>, langDescription:"Redux é uma biblioteca JavaScript de código aberto para gerenciar o estado do aplicativo. É comumente usado com bibliotecas como React ou Angular para criar interfaces de usuário.",},
   ]
 
